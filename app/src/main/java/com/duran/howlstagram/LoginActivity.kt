1d3a5fd0c662566be3b0203c80a494f5ee942ae6
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-11 11:43:20 +0900

    코드 주석달기 / password 입력 시 password가 보이지 않게 하기

--- app/src/main/java/com/duran/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/duran/howlstagram/LoginActivity.kt
@@ -32,44 +32,52 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        //Signup or Signin by email 버튼 클릭 시 회원가입 함수
         btn_email_login.setOnClickListener {
-            signinAndsignup()
+            signinAndSignup()
         }
     }
 
-    fun signinAndsignup(){
+    // 회원가입 함수
+    fun signinAndSignup() {
+        // 이메일 입력하는 부분과 비밀번호를 입력하는 부분을 넣어준다.
         auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
-            ?.addOnCompleteListener {
-                task ->
-                    if(task.isSuccessful){
-                        // Creating a user account
-                        moveMainPage(task.result.user)
-                    } else if(task.exception?.message.isNullOrEmpty()){
-                        // Show the error message
+            ?.addOnCompleteListener { // 회원가입한 결과값을 받아온다.// 회원가입한 결과값을 받아온다.
+                // 람다형식
+                    task ->
+                if (task.isSuccessful) {    //계정 만들기에 성공했을 때
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                } else {
+                    //Login if you have account
+                    if (task.exception?.message.equals("The email address is already in use by another account."))
+                        signinEmail() // 회원가입도 아니고 에러 메세지가 아닐 경우 로그인
+                    //Show the error message
+                    else
                         Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    } else {
-                        // Login if you have account
-                        signinEmail()
-                    }
+                }
             }
     }
 
-    fun signinEmail(){
+    // 로그인 함수
+    fun signinEmail() {
+        // 이메일 입력하는 부분과 비밀번호를 입력하는 부분을 넣어준다.
         auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
-            ?.addOnCompleteListener {
+            ?.addOnCompleteListener { // 회원가입한 결과값을 받아온다.
                     task ->
-                if(task.isSuccessful){
+                if (task.isSuccessful) { // 아이디와 비밀번호가 일치할 때
                     // Login
                     moveMainPage(task.result.user)
-                } else {
+                } else { // 아이디와 비밀번호가 불일치 할 때
                     // Show the error message
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if(user != null){
+    // 로그인 성공 시 다음 페이지 이동 함수
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) { // Firebase 유저 상태를 넘겨준다. / 유저 상태가 있을 경우 다음페이지로 이동
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
