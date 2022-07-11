d2a3f8079950049a44f1f8b827b89e4ba52662e0
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-11 16:39:17 +0900

    구글 로그인 구현 / 구글로그인 클릭 시 Firedase에 사용자가 등록안됨, 이메일과 비밀번호 입력하지 않고 sign버튼 클릭 시 app이 다운되는 에러 발생

--- app/src/main/java/com/duran/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/duran/howlstagram/LoginActivity.kt
@@ -3,33 +3,37 @@ package com.duran.howlstagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import com.duran.howlstagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private val btnEmailLogin: AppCompatButton by lazy {
+        findViewById(R.id.email_login_button)
+    }
+    private val etEmail: EditText by lazy {
+        findViewById(R.id.email_edittext)
+    }
+    private val etPassword: EditText by lazy {
+        findViewById(R.id.password_edittext)
+    }
+    private val googleLoginBtn: AppCompatButton by lazy {
+        findViewById(R.id.google_sign_in_button)
+    }
 
-    private val btn_email_login: AppCompatButton by lazy {
-        findViewById<AppCompatButton>(R.id.email_login_button)
-    }
-    private val et_email: EditText by lazy {
-        findViewById<EditText>(R.id.email_edittext)
-    }
-    private val et_password: EditText by lazy {
-        findViewById<EditText>(R.id.password_edittext)
-    }
-
-    var auth: FirebaseAuth? = null
-    var googleSignInClient: GoogleSignInClient? = null
-    var GOOGLE_LOGIN_CODE = 9001 // 구글 로그인할때 사용할 request code 
+    private var auth: FirebaseAuth? = null
+    private var googleSignInClient: GoogleSignInClient? = null
+    private var GOOGLE_LOGIN_CODE = 9001 // 구글 로그인할때 사용할 request code
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,23 +42,63 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         //Signup or Signin by email 버튼 클릭 시 회원가입 함수
-        btn_email_login.setOnClickListener {
+        btnEmailLogin.setOnClickListener {
             signinAndSignup()
         }
 
+        // 구글 로그인 버튼 클릭 시
+        googleLoginBtn.setOnClickListener {
+            // First step
+            googleLogin()
+        }
+
         // 구글 로그인 옵션 빌드
-       var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+       val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("416715023570-dp8naj20g669vq1pn8t0cs3nee6rj229.apps.googleusercontent.com") // 구글API키
             .requestEmail() // 구글 아이디
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso) // 옵션값을 구글로그인 클라이언트에 세팅
     }
 
+    // 구글 로그인 함수
+    private fun googleLogin(){
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            // 구글에서 넘겨주는 로그인 결과값을 받아온다.
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            // 성공했을때 이 결과값을 Firebase로 넘겨준다.
+            if (result!!.isSuccess){ // Nullsafety
+                val account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { // 회원가입한 결과값을 받아온다.
+                    task ->
+                if (task.isSuccessful) { // 아이디와 비밀번호가 일치할 때
+                    // Login
+                    moveMainPage(task.result?.user)
+                } else { // 아이디와 비밀번호가 불일치 할 때
+                    // Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
 
     // 회원가입 함수
-    fun signinAndSignup() {
+    private fun signinAndSignup() {
         // 이메일 입력하는 부분과 비밀번호를 입력하는 부분을 넣어준다.
-        auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+        auth?.createUserWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
             ?.addOnCompleteListener { // 회원가입한 결과값을 받아온다.// 회원가입한 결과값을 받아온다.
                 // 람다형식
                     task ->
@@ -73,9 +117,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     // 로그인 함수
-    fun signinEmail() {
+    private fun signinEmail() {
         // 이메일 입력하는 부분과 비밀번호를 입력하는 부분을 넣어준다.
-        auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+        auth?.signInWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
             ?.addOnCompleteListener { // 회원가입한 결과값을 받아온다.
                     task ->
                 if (task.isSuccessful) { // 아이디와 비밀번호가 일치할 때
@@ -89,7 +133,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     // 로그인 성공 시 다음 페이지 이동 함수
-    fun moveMainPage(user: FirebaseUser?) {
+    private fun moveMainPage(user: FirebaseUser?) {
         if (user != null) { // Firebase 유저 상태를 넘겨준다. / 유저 상태가 있을 경우 다음페이지로 이동
             startActivity(Intent(this, MainActivity::class.java))
         }
