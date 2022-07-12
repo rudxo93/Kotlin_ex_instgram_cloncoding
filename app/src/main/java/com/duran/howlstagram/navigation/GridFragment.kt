0acd5afc8a3a476cb2dc1a4d434b0767e37f82f6
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-12 19:02:40 +0900

    menu에 해당하는 fragment 생성

---
+++ app/src/main/java/com/duran/howlstagram/navigation/GridFragment.kt
@@ -0,0 +1,15 @@
+package com.duran.howlstagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.duran.howlstagram.R
+
+class GridFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_datail, container, false)
+        return view
+    }
+}
