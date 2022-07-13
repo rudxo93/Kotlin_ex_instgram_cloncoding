62d9107f527d2b52344a6cb2db1330b8717f0a5e
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-13 22:43:08 +0900

    main activity에 detail 적용 / 상세페이지가 main activity에 나타나지 않는다....

--- app/src/main/java/com/duran/howlstagram/MainActivity.kt
+++ app/src/main/java/com/duran/howlstagram/MainActivity.kt
@@ -21,35 +21,50 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigation.setOnNavigationItemSelectedListener(this)
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
+
+        // Set default screen
+        bottomNavigation.selectedItemId = R.id.action_home
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.action_home -> {
                 var detailViewFragment = DetailViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
                 return true
             }
             R.id.action_search -> {
                 var gridFragment = GridFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment)
+                    .commit()
                 return true
             }
             R.id.action_add_photo -> {
-                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
                     startActivity(Intent(this, AddPhotoActivity::class.java))
                 }
                 return true
             }
             R.id.action_favorite_alarm -> {
                 var alarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment)
+                    .commit()
                 return true
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment)
+                    .commit()
                 return true
             }
         }
--- app/src/main/java/com/duran/howlstagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/duran/howlstagram/navigation/DetailViewFragment.kt
@@ -10,28 +10,31 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.duran.howlstagram.R
-import com.duran.howlstagram.databinding.FragmentDatailBinding
 import com.duran.howlstagram.navigation.model.ContentDTO
 import com.google.firebase.firestore.FirebaseFirestore
 
-class DetailViewFragment: Fragment() {
-
-    lateinit var binding: FragmentDatailBinding
+class DetailViewFragment : Fragment() {
 
     lateinit var firestore: FirebaseFirestore
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_datail, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_datail, container, false)
 
         firestore = FirebaseFirestore.getInstance()
 
-        view.findViewById<RecyclerView>(R.id.detailviewfragment_recyclerview).adapter = DetailViewRecyclerViewAdapter()
-        view.findViewById<RecyclerView>(R.id.detailviewfragment_recyclerview).layoutManager = LinearLayoutManager(activity)
+        view.findViewById<RecyclerView>(R.id.detailviewfragment_recyclerview).adapter =
+            DetailViewRecyclerViewAdapter()
+        view.findViewById<RecyclerView>(R.id.detailviewfragment_recyclerview).layoutManager =
+            LinearLayoutManager(activity)
 
         return view
     }
 
-    inner class DetailViewRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
         var contentUidList: ArrayList<String> = arrayListOf()
@@ -39,23 +42,25 @@ class DetailViewFragment: Fragment() {
         init {
 
             // DB에 접근해서 데이터를 받아오는 쿼리 작성(시간순으로 받아온다.)
-            firestore?.collection("images")?.orderBy("itmestamp").addSnapshotListener { querySnaphsot, firebaseFirestoreException ->
-                // 받자마자 값 초기화
-                contentDTOs.clear()
-                contentUidList.clear()
-                // for문을 돌려서 스냅샷에 넘어오는 데이터들을 하나하나씩 읽어들인다.
-                for(snapshot in querySnaphsot!!.documents){
-                    // ContentDTO 방식으로 캐스팅한다.
-                    var item = snapshot.toObject(ContentDTO::class.java)
-                    contentDTOs.add(item!!)
-                    contentUidList.add(snapshot.id)
+            firestore.collection("images").orderBy("itmestamp")
+                .addSnapshotListener { querySnaphsot, firebaseFirestoreException ->
+                    // 받자마자 값 초기화
+                    contentDTOs.clear()
+                    contentUidList.clear()
+                    // for문을 돌려서 스냅샷에 넘어오는 데이터들을 하나하나씩 읽어들인다.
+                    for (snapshot in querySnaphsot!!.documents) {
+                        // ContentDTO 방식으로 캐스팅한다.
+                        val item = snapshot.toObject(ContentDTO::class.java)
+                        contentDTOs.add(item!!)
+                        contentUidList.add(snapshot.id)
+                    }
+                    notifyDataSetChanged() // 값이 새로고침 됨
                 }
-                notifyDataSetChanged() // 값이 새로고침 됨
-            }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
 
             return CustomViewHolder(view)
         }
@@ -69,23 +74,28 @@ class DetailViewFragment: Fragment() {
 
         // 서버에서 넘어온 데이터들을 맵핑시켜주는 부분
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var viewholder = (holder as CustomViewHolder).itemView // CustomViewHolder로 캐스팅
+            val viewholder = (holder as CustomViewHolder).itemView // CustomViewHolder로 캐스팅
 
 
             // UserId
-            viewholder.findViewById<TextView>(R.id.detailviewitem_profile_textview).text = contentDTOs!![position].userId
+            viewholder.findViewById<TextView>(R.id.detailviewitem_profile_textview).text =
+                contentDTOs[position].userId
 
             // Image
-            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.findViewById(R.id.detailviewitem_imageview_content))
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
+                .into(viewholder.findViewById(R.id.detailviewitem_imageview_content))
 
             //Explain of content
-            viewholder.findViewById<TextView>(R.id.detailviewitem_explain_textview).text = contentDTOs!![position].explain
+            viewholder.findViewById<TextView>(R.id.detailviewitem_explain_textview).text =
+                contentDTOs[position].explain
 
             // likes
-            viewholder.findViewById<TextView>(R.id.detailviewitem_favoritecounter_textview).text = "Likes " + contentDTOs!![position].favoriteCount
+            viewholder.findViewById<TextView>(R.id.detailviewitem_favoritecounter_textview).text =
+                "Likes " + contentDTOs!![position].favoriteCount
 
             // Proflie Image
-            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.findViewById(R.id.detailviewitem_profile_image))
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
+                .into(viewholder.findViewById(R.id.detailviewitem_profile_image))
         }
     }
 }
