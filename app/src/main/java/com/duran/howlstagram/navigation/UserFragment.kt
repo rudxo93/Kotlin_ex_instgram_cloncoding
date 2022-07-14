aba370c2fa6f4bf8f1b98eff58531f3a03e31c80
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-14 15:07:46 +0900

    유저페이즈 기능 구현

--- app/src/main/java/com/duran/howlstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/duran/howlstagram/navigation/UserFragment.kt
@@ -4,12 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.duran.howlstagram.R
+import com.duran.howlstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserFragment: Fragment() {
+
+    lateinit var fragmentView: View
+    lateinit var firestore: FirebaseFirestore
+    lateinit var uid: String
+    lateinit var auth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+        uid = arguments?.getString("destinationUid")!!
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        fragmentView.findViewById<RecyclerView>(R.id.account_recycleriew).adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView.findViewById<RecyclerView>(R.id.account_recycleriew).layoutManager = GridLayoutManager(activity, 3)
+        return fragmentView
+    }
+
+    inner class UserFragmentRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        val contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        init {
+            firestore.collection("images").whereEqualTo("uid", uid).addSnapshotListener { querySnapshot, FirebaseFirestoreException ->
+                // Sometimes, This code return null of querySnapshot when it signout
+                if(querySnapshot == null) return@addSnapshotListener
+
+                // Get data
+                for(snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                fragmentView.findViewById<TextView>(R.id.account_tv_post_count).text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val width = resources.displayMetrics.widthPixels / 3
+            val imageview = ImageView(parent.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageview)
+        }
     }
 }
