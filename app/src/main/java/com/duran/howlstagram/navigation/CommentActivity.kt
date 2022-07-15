f44e880d51508a0746f39d90e7241fb21b762abb
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-15 15:40:59 +0900

    댓글달기 클릭 후 댓글을 쓰고 send하게되면 database에서 comments 컬렉션이 생기고 여기에 메세지가 입력이 됨

--- app/src/main/java/com/duran/howlstagram/navigation/CommentActivity.kt
+++ app/src/main/java/com/duran/howlstagram/navigation/CommentActivity.kt
@@ -2,11 +2,42 @@ package com.duran.howlstagram.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.duran.howlstagram.R
+import com.duran.howlstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class CommentActivity : AppCompatActivity() {
+
+    private val commentBtnSend: Button by lazy {
+        findViewById(R.id.comment_btn_send)
+    }
+    private val commentEditMassage: EditText by lazy {
+        findViewById(R.id.comment_edit_message)
+    }
+
+    lateinit var contentUid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment)
+
+        contentUid = intent.getStringExtra("contentUid")!!
+
+        commentBtnSend.setOnClickListener {
+            var comment = ContentDTO.comment()
+            comment.userId = FirebaseAuth.getInstance().currentUser?.email
+            comment.uid = FirebaseAuth.getInstance().currentUser?.uid
+            comment.comment = commentEditMassage.text.toString()
+            comment.timestamp = System.currentTimeMillis()
+
+            FirebaseFirestore.getInstance().collection("images").document(contentUid!!).collection("comments").document().set(comment)
+
+            commentEditMassage.setText("")
+        }
+
     }
 }
--- app/src/main/java/com/duran/howlstagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/duran/howlstagram/navigation/DetailViewFragment.kt
@@ -1,5 +1,6 @@
 package com.duran.howlstagram.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -122,6 +123,11 @@ class DetailViewFragment : Fragment() {
                 bundle.putString("userId", contentDTOs[position].userId)
                 fragment.arguments = bundle
                 activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_content, fragment)?.commit()
+            }
+            viewholder.findViewById<ImageView>(R.id.detailviewitem_comment_imageview).setOnClickListener {  v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                startActivity(intent)
             }
         }
 
