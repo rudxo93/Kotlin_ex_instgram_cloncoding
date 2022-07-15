977cd37f204fcc6b737f67d2a7d14a404215c11a
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-15 16:49:07 +0900

    댓글 띄우기 기능 구현

--- app/src/main/java/com/duran/howlstagram/navigation/CommentActivity.kt
+++ app/src/main/java/com/duran/howlstagram/navigation/CommentActivity.kt
@@ -2,8 +2,16 @@ package com.duran.howlstagram.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.duran.howlstagram.R
 import com.duran.howlstagram.navigation.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
@@ -18,6 +26,9 @@ class CommentActivity : AppCompatActivity() {
     private val commentEditMassage: EditText by lazy {
         findViewById(R.id.comment_edit_message)
     }
+    private val commentRecyclerView: RecyclerView by lazy {
+        findViewById(R.id.comment_recyclerview)
+    }
 
     lateinit var contentUid: String
 
@@ -26,6 +37,9 @@ class CommentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_comment)
 
         contentUid = intent.getStringExtra("contentUid")!!
+
+        commentRecyclerView.adapter = CommentrecyclerviewAdapter()
+        commentRecyclerView.layoutManager = LinearLayoutManager(this)
 
         commentBtnSend.setOnClickListener {
             var comment = ContentDTO.comment()
@@ -38,6 +52,46 @@ class CommentActivity : AppCompatActivity() {
 
             commentEditMassage.setText("")
         }
+    }
 
+    inner class CommentrecyclerviewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        val comments: ArrayList<ContentDTO.comment> = arrayListOf()
+        init {
+            FirebaseFirestore.getInstance().collection("images").document(contentUid).collection("comments").orderBy("tiemstamp").addSnapshotListener { value, error ->
+                comments.clear()
+                if(value == null) return@addSnapshotListener
+
+                for (snapshot in value.documents){
+                    comments.add(snapshot.toObject(ContentDTO.comment::class.java)!!)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+
+            return CustomViewHolder(view)
+        }
+
+        private inner class CustomViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val view = holder.itemView
+            view.findViewById<TextView>(R.id.commentviewitem_textview_comment).text = comments[position].comment
+            view.findViewById<TextView>(R.id.commentviewitem_textview_profile).text = comments[position].userId
+
+            FirebaseFirestore.getInstance().collection("profileImages").document(comments[position].uid!!).get().addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val url = task.result["image"]
+                    Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.findViewById(R.id.commentviewitem_imageview_profile))
+                }
+            }
+        }
     }
 }
