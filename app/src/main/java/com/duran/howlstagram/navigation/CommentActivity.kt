2bd0850256d4495f0b7357f487de79c82be8025c
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-15 15:31:14 +0900

    댓글 달기 레이아웃 구성

---
+++ app/src/main/java/com/duran/howlstagram/navigation/CommentActivity.kt
@@ -0,0 +1,12 @@
+package com.duran.howlstagram.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.duran.howlstagram.R
+
+class CommentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
