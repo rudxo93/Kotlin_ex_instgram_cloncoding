3c276975e987b3b4fb541602876814f0dce1915b
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-19 17:50:47 +0900

    사진 업로드 기능 구현(주석 달기)

--- app/src/main/java/com/duran/howlstagram/AddPhotoActivity.kt
+++ app/src/main/java/com/duran/howlstagram/AddPhotoActivity.kt
@@ -1,12 +1,63 @@
 package com.duran.howlstagram
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.databinding.DataBindingUtil
+import com.duran.howlstagram.databinding.ActivityAddPhotoBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPhotoActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityAddPhotoBinding
+    lateinit var auth: FirebaseAuth
+    lateinit var storage: FirebaseStorage
+    lateinit var photoUri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_photo)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_photo)
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        // 엑티비티를 실행하자마자 화면이 열릴수 있도록 open code
+        var intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        photoResult.launch(intent)
+
+        // addPhoto버튼 클릭 시
+        binding.addphotoUploadBtn.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    fun contentUpload() {
+        // 이미지 저장 날싸와 시간(파일명이 중복되지 않도록 날짜와 시간으로)
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        // 파일 저장 이름 만들기 -> IMAGE_저장날짜.png
+        val imageFileName = "IMAGE_" + timeStamp + ".png"
+
+        // Firebase Storage에 images라는 폴더를 만들어서 사진을 업로드한다.
+        var storagePath = storage.reference.child("images").child(imageFileName)
+
+        // 파일 업로드
+        storagePath.putFile(photoUri).addOnSuccessListener {
+            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 선택한 이미지를 받는 부분
+    var photoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        // 사진 받는 부분
+        photoUri = result.data?.data!! // 이미지의 경로가 넘어온다. photoUri에 경로를 담아준다.
+        binding.uploadImageview.setImageURI(photoUri) // imageview에 선택한 이미지를 넣어준다.
     }
 }
--- app/src/main/java/com/duran/howlstagram/MainActivity.kt
+++ app/src/main/java/com/duran/howlstagram/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.duran.howlstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.duran.howlstagram.databinding.ActivityMainBinding
 import com.duran.howlstagram.fragment.AlarmFragment
@@ -20,6 +26,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+
+        // 사진 경로를 가져올 수 있는 권한 요청
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 0)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -38,6 +47,14 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             // addPhoto 아이템 -> photo upload
             R.id.action_add_photo -> {
+                // 외부 스토리지 경로를 가져올 수 있는 권한이 있는지 체크 후 addPhoto 액티비티를 호출할 수 있는 코드를 넣어준다.
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // 사진 읽기(READ_EXTERNAL_STORAGE)가 있을때
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                } else {
+                    // 사진 읽기(READ_EXTERNAL_STORAGE)가 없을때
+                    Toast.makeText(this, getString(R.string.donothave_permission), Toast.LENGTH_SHORT).show()
+                }
                 return true
             }
             // favoriteAlarm 아이템 -> 좋아요 알람
