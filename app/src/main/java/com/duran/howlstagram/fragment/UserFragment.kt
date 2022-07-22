13a4a82c7742a94876173cc64ec4f48319a79dc0
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-22 22:08:03 +0900

    다른 계정의 follow를 누르게 되면 database에 값이 들어간다(주석 달기) -> Firebase에서 follow와 following 확인

--- app/src/main/java/com/duran/howlstagram/fragment/UserFragment.kt
+++ app/src/main/java/com/duran/howlstagram/fragment/UserFragment.kt
@@ -21,6 +21,7 @@ import com.duran.howlstagram.R
 import com.duran.howlstagram.databinding.FragmentUserBinding
 import com.duran.howlstagram.databinding.ItemImageviewBinding
 import com.duran.howlstagram.model.ContentModel
+import com.duran.howlstagram.model.FollowModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
@@ -65,8 +66,10 @@ class UserFragment: Fragment() {
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
         // activity에서 넘어온 dUid 데이터 받기
+        // 현재 로그인한 사용자의 UID값
         dUid = arguments?.getString("dUid")!!
         // 내 계정인지 상대방 계정인지 판단
+        // 지금 사용자의 Uid 정보가 currentUid에 들어간다
         currentUid = auth.currentUser!!.uid
 
         binding.accountRecyclerview.adapter = UserFragmentRecyclerViewAdapter()
@@ -107,6 +110,10 @@ class UserFragment: Fragment() {
                 mainActivity.binding.bottomNavigation.selectedItemId = R.id.action_home
             }
 
+            // follow버튼을 눌렀을때 팔로우와 팔로잉에 대한 함수 호출
+            binding.accountBtnFollowSignout.setOnClickListener {
+                reqeustFollwerAndFollowing()
+            }
             // 본인 계정이 아니라면 마이 페이지라면 accountBtnFollowSignout 버튼을 signout으로
             binding.accountBtnFollowSignout.text = activity?.getText(R.string.follow)
         }
@@ -116,6 +123,84 @@ class UserFragment: Fragment() {
 
         return binding.root
     }
+
+    // 팔로우와 팔로잉 결과
+    fun reqeustFollwerAndFollowing() {
+        // 팔로잉 정보 -> 내가 따르고 있는 사람
+        // Firebase Database에 users라는 컬렉션에 사용자 UID명으로 저장한다. -> 데이터베이스 경로 접근
+        val tsDocFollowing = firestore.collection("users").document(currentUid)
+        // runTransaction
+        // -> 여러 클라이언트의 데이터 중복 접근 방지,  firestore에 저장하고 싶은 경로를 클래스로 만든뒤에 runTransaction을 호출
+        // 한 사람이 Document에 접근해서 데이터를 쓰는 동안
+        // 다른 사용자는 그동안 데이터베이스로 접근할 수 없다.
+        firestore.runTransaction {
+                transition ->
+            // followingModel = transition.get(데이터베이스 경로).toObject(데이터모델)
+            // 데이터베이스 경로에
+            var followingModel = transition.get(tsDocFollowing).toObject(FollowModel::class.java)
+            // followingModel == null -> 현재 팔로우를 한적이 없다.
+            if(followingModel == null){
+                // 팔로우 수가 0이다.
+                followingModel = FollowModel()
+                followingModel.followingCount = 1
+                followingModel.followings[dUid] = arguments?.getString("userId")!!
+                // transition.set(데이터베이스 경로, followingModel)
+                // users의 현재 사용자 이름으로된 데이터 베이스 경로에 followingCount와 followings값을 set해준다.
+                transition.set(tsDocFollowing,followingModel)
+                return@runTransaction
+                // FollowModel에 followings Map에 키값이 있는지 없는지 -> 있다면 true 없다면 false
+                // FollowModel에 followings Map에 dUid키값을 가져온다(true)
+                // 현재 dUid에는 팔로우 하고자 하는 계정의 주소가 담겨있다
+            }else if(followingModel.followings.containsKey(dUid)){
+                // 팔로우 취소
+                followingModel.followingCount = followingModel.followingCount - 1
+                // 팔로우를 취고하고자 하는 계정의 주소를 삭제한다.
+                followingModel.followings.remove(dUid)
+            }else{ // followingModel이 null이 아니거나 followingModel에 followings Map에 dUid키값이 없다면(false)
+                // 팔로우 카운터 +1
+                followingModel.followingCount = followingModel.followingCount + 1
+                // followings Map에 userId값을 dUid키값에 넣는다.
+                // 내가 팔로우 하고자 하는 계정의 이메일
+                followingModel.followings[dUid] = arguments?.getString("userId")!!
+
+            }
+            // transition.set(데이터베이스 경로, followingModel)
+            transition.set(tsDocFollowing,followingModel)
+            return@runTransaction
+        }
+
+        // 팔로우 정보 -> 나를 따르는 사람
+        // Firebase Database에 users라는 컬렉션에 dUid에 접근
+        val tsDocFollower = firestore.collection("users").document(dUid)
+        // 계정정보의
+        val sId = auth.currentUser?.email
+        firestore.runTransaction {
+                transition ->
+            // followModel = transition.get(데이터베이스 경로).toObject(데이터모델)
+            var followModel = transition.get(tsDocFollower).toObject(FollowModel::class.java)
+
+            if(followModel == null){
+                // followModel == null -> 아무도 팔로우를 하지 않았다
+                followModel = FollowModel()
+                followModel.followerCount = 1
+                followModel.followers[currentUid] = sId!!
+                transition.set(tsDocFollower,followModel)
+                return@runTransaction
+            }else if(followModel.followers.containsKey(currentUid)){
+                //로그인된 아이디가 누구를 스토킹을 이미 했을때
+                followModel.followerCount = followModel.followerCount - 1
+                followModel.followers.remove(currentUid)
+
+            }else{
+                //아직 아무도 스토킹을 하지 않았을때(인기가 있어서 상대방이 쫒아다닐때)
+                followModel.followerCount = followModel.followerCount + 1
+                followModel.followers[currentUid] = sId!!
+            }
+            transition.set(tsDocFollower,followModel)
+            return@runTransaction
+        }
+    }
+
 
     fun getProfileImage(){
         // Firebase의 profileImages컬렉션에 있는 프로필이미지를 가져온다.
--- app/src/main/java/com/duran/howlstagram/model/FollowModel.kt
+++ app/src/main/java/com/duran/howlstagram/model/FollowModel.kt
@@ -1,8 +1,12 @@
 package com.duran.howlstagram.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class FollowModel(
     var followerCount: Int = 0, // 팔로우 수
-    var followers: MutableMap<String, Boolean> = HashMap(), // 중복 팔로우 방지
+    var followers: MutableMap<String, String> = hashMapOf(), // 중복 팔로우 방지
     var followingCount: Int = 0, // 팔로잉 수
-    var followings: MutableMap<String, Boolean> = HashMap() // 중복 팔로잉 방지
-)
+    var followings: MutableMap<String, String> = hashMapOf() // 중복 팔로잉 방지
+) : Parcelable
