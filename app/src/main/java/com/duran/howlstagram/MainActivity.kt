c7a77a4e7f335079ebd9309bf145df8fc587fe1a
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-25 16:11:06 +0900

    로그인이 되자마자 DB에 pushToken을 저장한다. -> Firebase Database에 pushtokens컬렉션 생성 후 토큰을 저장한다

--- app/src/main/java/com/duran/howlstagram/MainActivity.kt
+++ app/src/main/java/com/duran/howlstagram/MainActivity.kt
@@ -19,15 +19,22 @@ import com.duran.howlstagram.fragment.GridFragment
 import com.duran.howlstagram.fragment.UserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener  {
 
     lateinit var binding: ActivityMainBinding
     lateinit var auth: FirebaseAuth
+    lateinit var message: FirebaseMessaging
+    lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        message = FirebaseMessaging.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
         // 메인 엑티비티에 detail fragment 적용
@@ -35,6 +42,27 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         // 사진 경로를 가져올 수 있는 권한 요청
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 0)
+        // Firebase Database에 pushToken을 저장한다.
+        // 로그인이 되자마자 바로 DB에 저장된다.
+        saveMyPushToken()
+    }
+
+    // Firebase Database에 pushToken을 저장한다.
+    fun saveMyPushToken() {
+        // 토큰 생성
+        message.token.addOnCompleteListener { // addOnCompleteListener 리스너로 성공 유무의 값을 확인
+            task ->
+            if (task.isSuccessful) {
+                // task에서 받아오는 토큰id
+                var token = task.result
+                // DB에 넣어준다.
+                var map = mutableMapOf<String, Any>()
+                map["token"] = token
+
+                // pushtokens컬렉션 안에 토큰값(map)을 넣어준다.
+                firestore.collection("pushtokens").document(auth.uid!!).set(map)
+            }
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
