2245ddcb272cd7a28e04c507b61ffb035f2d302a
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-25 09:27:16 +0900

    게시물에 댓글 달기 기능 구현

--- app/src/main/java/com/duran/howlstagram/CommentActivity.kt
+++ app/src/main/java/com/duran/howlstagram/CommentActivity.kt
@@ -3,8 +3,13 @@ package com.duran.howlstagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.duran.howlstagram.databinding.ActivityCommentBinding
+import com.duran.howlstagram.databinding.ItemCommentBinding
 import com.duran.howlstagram.model.ContentModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -24,6 +29,9 @@ class CommentActivity : AppCompatActivity() {
         // dUid : 컬렉션에 아이템 이름 -> dUid명으로 넘어온 intent안에 images컬렉션 안에 들어있는 아이템의 이름이 담겨있다.
         dUid = intent.getStringExtra("dUid")!!
 
+        binding.commentRecyclerview.adapter = CommentAdapter()
+        binding.commentRecyclerview.layoutManager = LinearLayoutManager(this)
+
         // send버튼 클릭 시
         binding.sendBtn.setOnClickListener {
             // Model의 comment에 set해준다.
@@ -38,6 +46,44 @@ class CommentActivity : AppCompatActivity() {
 
             binding.commentEdittext.setText("")
 
+        }
+    }
+
+    inner class ItemCommentViewHolder(var binding: ItemCommentBinding): RecyclerView.ViewHolder(binding.root)
+    inner class CommentAdapter: RecyclerView.Adapter<ItemCommentViewHolder>(){
+        var comments = arrayListOf<ContentModel.Comment>()
+
+        init {
+            // Database의 images컬렉션에 dUid의 문서 접근 -> comments컬렉션을 실시간 감시
+            firestore.collection("images").document(dUid).collection("comments").addSnapshotListener { value, error ->
+                // comments 초기화
+                comments.clear()
+
+                // 값이 null이라면 다시 감시
+                if(value == null) return@addSnapshotListener
+
+                // item을 value의 문서만큼 반복문
+                for(item in value.documents){
+                    comments.add(item.toObject(ContentModel.Comment::class.java)!!)
+                }
+                // 리스트의 크기와 아이템이 둘 다 변경되는 경우 사용
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemCommentViewHolder {
+            var view = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ItemCommentViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ItemCommentViewHolder, position: Int) {
+            var view = holder.binding
+            view.profileTextview.text = comments[position].userId
+            view.messageTextview.text = comments[position].comment
+        }
+
+        override fun getItemCount(): Int {
+            return comments.size
         }
     }
 
